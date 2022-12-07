7391696c508990ae67dbe74f8d75f6b52c1a1ff6
brandonvox <brandonvox9@gmail.com>
2022-12-08 04:16:45 +0700

    First commit

---
+++ Source/EldenRingLike/EldenRingLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EldenRingLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EldenRingLike, "EldenRingLike" );
