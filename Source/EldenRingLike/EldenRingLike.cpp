ecdf01936c8ee654140bcc5aacfa1e55c0166443
brandonvox <brandonvox9@gmail.com>
2022-12-08 04:25:52 +0700

    Revert "First commit"
    This reverts commit 7391696c508990ae67dbe74f8d75f6b52c1a1ff6.

--- Source/EldenRingLike/EldenRingLike.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "EldenRingLike.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EldenRingLike, "EldenRingLike" );
